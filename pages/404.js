d8c25722fd49553f7951f6827e8e31d366469fb5
MAKCIMELIANO <makcimeliano@gmail.com>
2022-09-29 14:12:53 +0300

    commit-test

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Heading from "../components/Heading";
+
+const Error = () => {
+  return (
+    <div>
+      <Heading text="404" />
+      <Heading tag="h2" text="Что-то пошло не так" />
+    </div>
+  );
+};
+
+export default Error;
---
+++ pages/posts.js
@@ -0,0 +1,12 @@
+import Heading from "../components/Heading";
+
+const Posts = () => {
+  return (
+    <div>
+      <Heading text="Posts list:" />
+      <p>Lorem ipsum dolor sit amet, condectetur adip</p>
+    </div>
+  );
+};
+
+export default Posts;
